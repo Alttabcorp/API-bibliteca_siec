8ce971bfdde7aec82441e4b998626c82dd1ff567
Glaymar A de França <glaymar2010@live.com>
2024-05-18 02:52:31 -0300

    :card_file_box: raw: Definir estrutura dos dados do banco(USER e BOOK)

--- src/db/migrations/1716011166350-book-table.ts
+++ src/db/migrations/1716011166350-book-table.ts
@@ -3,9 +3,20 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class BookTable1716011166350 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+        await queryRunner.query(`
+            CREATE TABLE book(
+                id uuid NOT NULL DEFAULT uuid_generate_v4(),
+                title varchar(256) NOT NULL,
+                description varchar(512) NOT NULL,
+                CONSTRAINT task_pk PRIMARY KEY (id)
+
+            );
+        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE IF EXISTS book;`)
     }
 
 }
